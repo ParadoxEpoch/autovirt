82298757b42d080a6baa73d3a571a0e2243e2555
Tristan Gauci <tristan@maega.com.au>
2022-11-01 00:17:58 +1100

    CLI is now interactive

---
+++ func/common.js
@@ -0,0 +1,68 @@
+const chalk = require('chalk');
+const {exec, spawn} = require("child_process");
+
+const msg = {
+    bold: chalk.bold,
+    info: chalk.bold.blue,
+    link: chalk.underline.blue,
+    //error: chalk.bgRed.hex('#cccccc'),
+    error: chalk.bold.red,
+    warn: chalk.bold.yellow,
+    success: chalk.bold.green,
+    brand: chalk.bold.hex('#7f00ff')
+}
+exports.msg = msg;
+
+// * Adds padding to both side of a string to center align it to the ASCII logo in the console
+function centerString(text) {
+    if (text >= 40) return text; // If text is too long to center, do nothing.
+    const padding = Math.floor((40 - text.length) / 2);
+    return ' '.repeat(padding) + text + ' '.repeat(padding);
+}
+
+exports.printLogo = function(text, style) {
+    console.clear();
+    console.log('                                        ');
+    console.log(msg.brand('               ___  _____               '));
+    console.log(msg.brand('              /  / /  _  \\             '));
+    console.log(msg.brand('             /  / /  / \\  \\           '));
+    console.log(msg.brand('            /  / /  /   \\  \\          '));
+    console.log(msg.brand('           /  / /  /     \\  \\         '));
+    console.log(msg.brand('          /  / /  /       \\  \\        '));
+    console.log(msg.brand('         /__/ /__/         \\__\\       '));
+    console.log('                                        ');
+    console.log('----------------------------------------');
+    console.log(msg[style || 'bold'](centerString(text || 'Nebula Harmony v0.1.0')));
+    console.log('----------------------------------------');
+    console.log('                                        ');
+}
+
+exports.sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+exports.epochToDuration = (ms) => {
+    const minutes = Math.floor(ms / 60000);
+    const seconds = ((ms % 60000) / 1000).toFixed(0);
+    return (
+        seconds === 60
+            ? (minutes + 1) + ':00'
+            : minutes + ':' + (seconds < 10 ? '0' : '') + seconds
+    );
+}
+
+// Executes a shell command. Inherits stdio if verbose=true, so output is shown and input is interactive.
+exports.shellExec = async (shellCmd, shellArgs = [], verbose = false) => {
+    const result = await new Promise(async function(resolve, reject) {
+        const spawnArgs = verbose ? {stdio: 'inherit'} : {}
+        let stdoutData = '';
+        const script = spawn(shellCmd, shellArgs, spawnArgs);
+        script.on("data", data => stdoutData += data);
+        script.on('close', (code) => {
+            resolve({
+                code: code,
+                stdout: stdoutData
+            });
+        });
+
+    });
+    return result;
+};
---
+++ func/sysinfo.js
@@ -0,0 +1,94 @@
+const info = require('systeminformation');
+
+const getSystemParam = {
+
+    uuid: async () => {
+        console.log('Getting System UUID...');
+        return await info.uuid();
+    },
+
+    bios: async () => {
+        console.log('Getting BIOS Information...');
+        return await info.bios();
+    },
+
+    mobo: async () => {
+        console.log('Getting Motherboard Information...');
+        return await info.baseboard();
+    },
+
+    cpu: async () => {
+        console.log('Getting CPU Information...');
+        return await info.cpu();
+    },
+
+    ram: async () => {
+        console.log('Getting RAM Information...');
+        const memInfo = await info.mem();
+        const memLayout = await info.memLayout();
+        return {
+            ...memInfo,
+            layout: memLayout,
+            dimms: memLayout.length,
+            speed: memLayout[0].clockSpeed,
+            manufacturer: memLayout[0].manufacturer,
+            model: memLayout[0].partNum,
+            totalGB: Math.ceil(memInfo.total / 1073741824), // RAM total in bytes / (1024 * 1024 * 1024), rounded up to nearest integer. Should give us total RAM in GB.)
+            dimmSizeGB: Math.ceil(memLayout[0].size / 1073741824)
+        }
+    },
+
+    gpu: async () => {
+        console.log('Getting GPU Information...');
+        const graphics = await info.graphics();
+        if (graphics.controllers.length > 1) graphics.controllers[0].isMultiGPU = true;
+        return graphics.controllers[0];
+    },
+
+    displays: async () => {
+        console.log('Getting Display Information...');
+        const graphics = await info.graphics();
+        return graphics.displays;
+    },
+
+    os: async () => {
+        console.log('Getting OS Information...');
+        return await info.osInfo();
+    },
+
+    disks: async () => {
+        console.log('Getting Disk Information...');
+        
+        let logical = JSON.parse(mountInfo.raw);
+        if (!Array.isArray(logical)) logical = [logical];
+
+        let physical = JSON.parse(diskInfo.raw);
+        if (!Array.isArray(physical)) physical = [physical];
+
+        return {
+            physical: physical,
+            logical: logical
+        }
+    }
+
+}
+
+async function getAllParams() {
+
+    const paramList = Object.keys(getSystemParam);
+    const output = {};
+
+    for (const param of paramList) {
+        output[param] = await getSystemParam[param]();
+    }
+
+    return output;
+
+}
+
+module.exports = {
+    get: {
+        ...getSystemParam,
+        all: getAllParams
+    }
+}
